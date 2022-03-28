a4db7855d9c49bad6e2f196ab960176743752127
Wouter van Deventer <wouter@Wouters-MacBook-Air.local>
2022-03-28 16:58:07 +0200

    Add new video and update bio

--- components/About.tsx
+++ components/About.tsx
@@ -1,21 +1,22 @@
-import Container from './Container'
-import SectionHeading from './SectionHeading'
-import Image from 'next/image'
-import bio from '../public/images/bio.jpeg'
-import containerStyles from '../styles/components/Container.module.scss'
-import styles from '../styles/components/About.module.scss'
+import Container from "./Container";
+import SectionHeading from "./SectionHeading";
+import Image from "next/image";
+import bio from "../public/images/bio.jpeg";
+import containerStyles from "../styles/components/Container.module.scss";
+import styles from "../styles/components/About.module.scss";
 
 const About = () => {
   return (
     <Container id="about">
       <SectionHeading title="About" />
       <p>
-        Koen van der Linden is a Dutch jazz piano player and singer. He grew up
-        in the Hague where he started playing piano at the age of ten and at the
-        age of sixteen he discovered the city&apos;s vibrant jazz scene, after
-        which he decided he wanted to dedicate his life fully to music. Two
-        years later he got admitted to the Royal Conservatory of The Hague.
+        Koen van der Linden is a Royal Conservatory graduate in jazz piano, and
+        a self taught trumpet player. He is based in the Hague and plays around
+        in the Dutch jazz scene and with his own trio. In his playing you can
+        find a sincere love for old jazz standards, next to a wide range of
+        modern day influences and above all, a great passion.
       </p>
+
       <div className={`${containerStyles.full_width} ${styles.image}`}>
         <Image
           src={bio}
@@ -25,21 +26,33 @@ const About = () => {
           lazyBoundary="1000px"
         />
       </div>
+
       <p>
-        At the Royal Conservatory Koen discovered the ins and outs of jazz
-        music, and started getting interested in other styles of music as well,
-        like impressionism and neo soul. Next to that he started composing and
-        experimenting with different groups with which he started playing around
-        in the Dutch jazz scene.
+        Koen was introduced to the piano by his grandfather, who taught him the
+        first basics at the age of ten. Soon after that he was getting regular
+        piano lessons, where he started improvising and composing his first
+        tunes. At the age of sixteen, Koen got in touch with the vibrant jazz
+        scene of the Hague. He started going out to jam sessions and concerts,
+        where he listened and played along. At the age of nineteen, Koen was
+        admitted to the Royal Conservatory.
       </p>
+
       <p>
-        In 2021 he graduated from his bachelor jazz piano, and started working
-        with Dani Font, Cas Jiskoot and Tim Hennekes to form a quartet with
-        which he played his original compositions and experimented with new
-        grooves and style combinations.
+        While studying at the Conservatory, Koen started to develop his own
+        sound and style. You can find elements of Duke Ellington, Bud Powell and
+        Roy Hargrove in his playing. Next to that, he started working with
+        different musicians at the conservatory and started learning how to
+        interact with his fellow colleagues. He also took up the trumpet with
+        which he started experimenting and playing around.
+      </p>
+
+      <p>
+        Koen graduated in 2022 and has been an active, promising member of the
+        Dutch jazz scene ever since. modern day influences and above all, a
+        great passion.
       </p>
     </Container>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- components/Videos.tsx
+++ components/Videos.tsx
@@ -1,15 +1,16 @@
-import Container from './Container'
-import SectionHeading from './SectionHeading'
-import Video from './Video'
+import Container from "./Container";
+import SectionHeading from "./SectionHeading";
+import Video from "./Video";
 
 const Videos = () => {
   return (
     <Container id="video">
       <SectionHeading title="Video" />
+      <Video src="evUmlB3470Y" />
       <Video src="mTJpUdCoKYI" />
       <Video src="t3Pr0nW9tZU" />
     </Container>
-  )
-}
+  );
+};
 
-export default Videos
+export default Videos;
