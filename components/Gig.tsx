e1b9b40a88890e712b08601d090634b797edc90f
Wouter van Deventer <woutervdeventer@gmail.com>
2022-05-24 09:33:41 +0200

    Add new videos and add city to events

--- components/Gig.tsx
+++ components/Gig.tsx
@@ -5,37 +5,27 @@ interface Props {
   event: Event
 }
 
-const months = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec'
-]
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 export default function Gig({ event }: Props) {
   const date = new Date(event.start.date || event.start.dateTime)
   const dateTime = event.start.dateTime
   const day = date.getDate().toString()
   const month = months[date.getMonth()]
-  const hours =
-    dateTime && new Date(dateTime).getHours().toString().padStart(2, '0')
-  const minutes =
-    dateTime && new Date(dateTime).getMinutes().toString().padStart(2, '0')
+  const hours = dateTime && new Date(dateTime).getHours().toString().padStart(2, '0')
+  const minutes = dateTime && new Date(dateTime).getMinutes().toString().padStart(2, '0')
   const time = dateTime && `${hours}:${minutes}`
-  const venue = event.location?.split(',')[0]
-  // .filter((v, i) => i === 0 || i === 2)
 
-  // const venue = address?.[0]
+  const locationStr = event.location
+  let city
+  let venue
 
-  // const location = address && `${venue}`
+  if (locationStr) {
+    city = getCityFromLocationString(locationStr)
+    venue = getVenueFromLocationString(locationStr)
+  }
+
+  const location = makeLocation(city, venue)
 
   return (
     <article className={styles.gig}>
@@ -44,12 +34,48 @@ export default function Gig({ event }: Props) {
         <div className={styles.day}>{day}</div>
         <div className={styles.time}>{time ?? 'T.B.A.'}</div>
       </div>
-      <div className={styles.info}>
-        {event.description ?? 'Info to be announced'}
-      </div>
-      <div className={styles.location}>
-        {venue ?? 'Location to be announced'}
-      </div>
+      <div className={styles.info}>{event.description ?? 'Info to be announced'}</div>
+      <div className={styles.location}>{location}</div>
     </article>
   )
+}
+
+function getCityFromLocationString(locationStr: string): string | undefined {
+  let city
+
+  const cityRegExp = /[A-Z][a-z](?:.*)/
+  const strArr = locationStr.split(',')
+
+  if (strArr.length === 2) {
+    city = strArr[0]
+  }
+
+  if (strArr.length === 3) {
+    city = strArr[1].match(cityRegExp)?.[0]
+  }
+
+  if (strArr.length === 4) {
+    city = strArr[2].match(cityRegExp)?.[0]
+  }
+
+  return city
+}
+
+function getVenueFromLocationString(locationStr: string): string | undefined {
+  let venue
+
+  const strArr = locationStr.split(',')
+
+  if (strArr.length === 4 || strArr.length === 3) {
+    venue = strArr[0]
+  }
+
+  return venue
+}
+
+function makeLocation(city: string | undefined, venue: string | undefined): string {
+  if (city && !venue) return city
+  if (!city && venue) return venue
+  if (city && venue) return `${venue}, ${city}`
+  return 'Location to be announced'
 }
--- components/Videos.tsx
+++ components/Videos.tsx
@@ -1,16 +1,19 @@
-import Container from "./Container";
-import SectionHeading from "./SectionHeading";
-import Video from "./Video";
+import Container from './Container'
+import SectionHeading from './SectionHeading'
+import Video from './Video'
 
 const Videos = () => {
   return (
-    <Container id="video">
-      <SectionHeading title="Video" />
-      <Video src="evUmlB3470Y" />
-      <Video src="mTJpUdCoKYI" />
-      <Video src="t3Pr0nW9tZU" />
+    <Container id='video'>
+      <SectionHeading title='Video' />
+      <Video src='TM9cP2pTgkU' />
+      <Video src='N1P5y2v-S9s' />
+      <Video src='sF8sajNiPNE' />
+      <Video src='7SRLdfdUTDY' />
+      <Video src='mTJpUdCoKYI' />
+      <Video src='t3Pr0nW9tZU' />
     </Container>
-  );
-};
+  )
+}
 
-export default Videos;
+export default Videos
